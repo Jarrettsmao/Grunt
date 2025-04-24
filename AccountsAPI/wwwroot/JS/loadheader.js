42993338c4e3d8898f0929f069d9ce829cb531d6
Jarrett Mao <53239875+Jarrettsmao@users.noreply.github.com>
2025-04-23 23:52:39 -0700

    header css

--- AccountsAPI/wwwroot/JS/loadheader.js
+++ AccountsAPI/wwwroot/JS/loadheader.js
@@ -4,5 +4,25 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             // console.log("header content", data);
             document.getElementById('header-placeholder').innerHTML = data;
+
+            const navLinks = document.getElementById('nav-links');
+            const token = sessionStorage.getItem("token");
+
+            if (navLinks){
+                if (token){
+                    navLinks.innerHTML = `
+                    <a href="/HTML/accountpage.html">Account</a>
+                    `;
+                    console.log(token);
+                } else {
+                    navLinks.innerHTML = `
+                    <a href="/HTML/login.html" class="login">Login</a>
+                    <a href="/HTML/signup.html" class="signup">Sign Up</a>
+                    `;
+                }
+            }
+        })
+        .catch(function(error){
+            console.error("Failed to load header:", error);
         });
 });
